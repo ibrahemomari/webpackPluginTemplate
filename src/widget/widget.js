ad8dcfb7609115221a0c4a7ef3b04339ceee2b30
AlexKvazos <contact@alexkvazos.com>
2017-10-19 18:04:47 -0500

    Add plugin-template-webpack

---
+++ src/widget/widget.js
@@ -0,0 +1,9 @@
+var buildfire = require('buildfire');
+
+// This is the entry point of your plugin's widget.
+// Feel free to require any local or npm modules you've installed.
+
+buildfire.alert.create({
+  title: 'Hello from BuildFire!',
+  message: 'This alert is being launched from the widget'
+});

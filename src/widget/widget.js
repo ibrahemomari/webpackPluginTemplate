13220acd6bed6900bda2c72ac40cd97384dee3c9
AlexKvazos <contact@alexkvazos.com>
2018-02-21 19:11:21 -0600

    Use proper alert method

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -3,7 +3,7 @@ var buildfire = require('buildfire');
 // This is the entry point of your plugin's widget.
 // Feel free to require any local or npm modules you've installed.
 
-buildfire.alert.create({
+buildfire.notifications.alert({
   title: 'Hello from BuildFire!',
   message: 'This alert is being launched from the widget'
 });

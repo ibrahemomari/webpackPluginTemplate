6532e322c682c224b9ca275ea50211802fa5dc06
Daniel Hindi <Daniel@MadainCorp.com>
2018-06-07 15:52:44 -0700

    remove alert

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -2,8 +2,9 @@ var buildfire = require('buildfire');
 
 // This is the entry point of your plugin's widget.
 // Feel free to require any local or npm modules you've installed.
-
+/*
 buildfire.notifications.alert({
   title: 'Hello from BuildFire!',
   message: 'This alert is being launched from the widget'
 });
+*/
